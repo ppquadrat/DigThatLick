3473677003ee7d11521fd64fa5f2bbeddba56a8b
ppquadrat <proutskova@googlemail.com>
2021-05-09 01:58:06 +0100

    patching adds possible solo performers for a list; logging bugs fixed; modularised

--- patch_metadata.py
+++ patch_metadata.py
@@ -18,10 +18,10 @@ import dtlutil
 
 # logging
 import logging
-MIN_LEVEL = logging.INFO
+MIN_LEVEL = logging.DEBUG
 dtlutil.setup_log(MIN_LEVEL)
 
-##############################################################
+#%%#############################################################
 # read in rdf graph
 
 import rdflib
@@ -40,7 +40,7 @@ dtlutil.read_in_rdf(g, RDFfile)
 
 logging.debug("\ngraph has %i triples", len(g))
 
-##############################################################
+#%%#############################################################
 
 import uuid
 
@@ -76,7 +76,65 @@ def create_performer(musicianURI, instrumentURI):
                   g.value(musicianURI, FOAF.name), g.value(instrumentURI, DTL.dtl_inst_label) )
     return performerURI
 
-##############################################################
+
+def get_instrument(instrument):
+    instrumentURI = g.value(subject=None, predicate=DTL.orig_inst_label, \
+                                object=Literal(instrument), default=None, any=False)
+    if instrumentURI == None:
+        instrumentURI = create_instrument(instrument)
+    else:
+        logging.debug("found instrument %s", instrument)
+    return instrumentURI
+
+def get_musician(musician):
+    musicianURI = None
+    for uri in g.subjects(predicate=FOAF.name, object=Literal(musician)):
+        if not (uri, RDF.type, MO.MusicGroup) in g:
+            musicianURI = uri
+            
+   # musicianURI = g.value(subject=None, predicate=FOAF.name, \
+   #                                 object=Literal(musician), default=None, any=True)
+    if musicianURI == None:
+         musicianURI = create_artist(musician)
+    else:
+        logging.debug("found musician %s", musician)
+    return musicianURI
+
+def get_performer(performanceURI, musicianURI, instrumentURI):
+    performerURI = None
+    for performer in g.objects(performanceURI, MO.performer):
+        perf_mus = g.value(performer, DTL.musician)
+        perf_inst = g.value(performer, DTL.instrument)
+        if perf_mus == musicianURI and perf_inst == instrumentURI:
+            performerURI = performer
+            break
+    if performerURI != None:
+                  logging.debug("performer %s playing %s already exists for this performance", \
+                                  g.value(perf_mus, FOAF.name), g.value(perf_inst, DTL.dtl_inst_label) )
+    else:
+        performerURI = create_performer(musicianURI, instrumentURI)
+  
+    return performerURI
+
+
+def add_possible_solo_performer(performanceURI, solo_performance_uri, musicianURI, instrumentURI):
+    performerURI = get_performer(performanceURI, musicianURI, instrumentURI)
+    if performerURI == None:
+        performerURI = create_performer(musicianURI, instrumentURI)
+    g.add( (solo_performance_uri, DTL.possible_solo_performer, performerURI) )
+    logging.info("Added possible solo performer %s playing %s", \
+                  g.value(musicianURI, FOAF.name), g.value(instrumentURI, DTL.dtl_inst_label) )
+
+def remove_possible_solo_performers(solo_performance_uri):
+    for psperf in g.objects(solo_performance_uri, DTL.possible_solo_performer):
+        psmusician = g.value(psperf, DTL.musician)
+        logging.debug("removing possible solo performer %s", g.value(psmusician, FOAF.name))
+    tcount = len(g)
+    g.remove( (solo_performance_uri, DTL.possible_solo_performer, None) )
+    if tcount - len(g) > 0:
+        logging.debug("%i triples removed", tcount - len(g))
+
+#%%#############################################################
 
 import csv
 
@@ -96,92 +154,77 @@ with open(PatchMetadataFile, 'r') as csvfile:
 
             logging.info("\nPatching solo performance for track %s, band %s, date %s", row[4], row[2], row[7])
 
-            if ',' in musician:
-                logging.info("too many musicians given: %s", musician)
-                logging.info("skipping")
+
+            # get solo performance
+            solo_performance_uri = g.value(subject=None, predicate=DTL.solo_id, \
+                                object=Literal(solo_id), default=None, any=False)
+
+            if solo_performance_uri == None:
+                logging.warning("Solo id %s not found for track %s", solo_id, str(row))
             else:
+                logging.debug("found solo for id %s", solo_id)
+                
+                # get performance
+                performanceURI = g.value(subject=None, predicate=EVENT.sub_event, \
+                                object=solo_performance_uri, default=None, any=False)
 
-                # get solo performance
-                solo_performance_uri = g.value(subject=None, predicate=DTL.solo_id, \
-                                    object=Literal(solo_id), default=None, any=False)
-
-                if solo_performance_uri == None:
-                    logging.warning("Solo id %s not found for track %s", solo_id, str(row))
+                if performanceURI == None:
+                    logging.warning("Can't find performance for solo id %s", solo_id)
                 else:
-                    logging.debug("found solo for id %s", solo_id)
+                    perf_title = str(g.value(performanceURI, DC.title))
+                    logging.debug("found performance with title: %s", perf_title)
+                    if perf_title != track_title:
+                        logging.warning("patch track title %s differs from found performance title %s", track_title, perf_title)
+        
+                    # get instrument
+                    instrumentURI = get_instrument(instrument)
                     
-                    # get performance
-                    performanceURI = g.value(subject=None, predicate=EVENT.sub_event, \
-                                    object=solo_performance_uri, default=None, any=False)
+                    # set solo instrument
+                    solo_instrument = g.value(solo_performance_uri, DTL.solo_instrument)
+                    solo_inst_label = g.value(solo_instrument, DTL.dtl_inst_label)
+                    if solo_instrument != instrumentURI:
+                        logging.info("solo instrument in the repository is %s, in the patchfile it is %s -- patching", \
+                                        solo_inst_label, instrument)
+                        g.set( (solo_performance_uri, DTL.solo_instrument, instrumentURI) )
 
-                    if performanceURI == None:
-                        logging.warning("Can't find performance for solo id %s", solo_id)
-                    else:
-                        perf_title = str(g.value(performanceURI, DC.title))
-                        logging.debug("found performance with title: %s", perf_title)
-                        if perf_title != track_title:
-                            logging.warning("patch track title %s differs from found performance title %s", track_title, perf_title)
-            
-                        # get musician
-                        musicianURI = g.value(subject=None, predicate=FOAF.name, \
-                                    object=Literal(musician), default=None, any=True)
-                        if musicianURI == None:
-                             musicianURI = create_artist(musician)
-                        else:
-                            logging.debug("found musician %s", musician)
-
-                        # get instrument
-                        instrumentURI = g.value(subject=None, predicate=DTL.orig_inst_label, \
-                                    object=Literal(instrument), default=None, any=False)
-                        if instrumentURI == None:
-                            instrumentURI = create_instrument(instrument)
-                        else:
-                            logging.debug("found instrument %s", instrument)
-
-                        # create new performer
-                        perfomerURI = None
-                        for performer in g.objects(performanceURI, MO.performer):
-                            perf_mus = g.value(performer, DTL.musician)
-                            perf_inst = g.value(performer, DTL.instrument)
-                            if perf_mus == musicianURI and perf_inst == instrumentURI:
-                                performerURI = performer
-                                break
-
-                        if perfomerURI != None:
-                            logging.warning("performer %s playing %s already exists for this performance", \
-                                            g.value(perf_mus, FOAF.name), g.value(perf_inst, DTL.dtl_inst_label) )
-
-                        else:
-                            performerURI = create_performer(musicianURI, instrumentURI)
-
-                        # check solo instrument
-                        solo_instrument = g.value(solo_performance_uri, DTL.solo_instrument)
-                        solo_inst_label = g.value(solo_instrument, DTL.dtl_inst_label)
-                        if solo_instrument != instrumentURI:
-                            logging.info("solo instrument in the repository is %s, in the patchfile it is %s -- patching", \
-                                            solo_inst_label, instrument)
-                            g.set( (solo_performance_uri, DTL.solo_instrument, instrumentURI) )
-
+                    # musician(s)
+                    if ',' in musician:
+                        # this is a list of possible_solo_performers
+                        logging.info("adding possible solo performers: %s", musician)
+                        logging.debug("deleting existing possible solo performers")
+                        remove_possible_solo_performers(solo_performance_uri)
+                        musicians = musician.split(',')
+                        for musician in musicians:
+                            musician = musician.strip()
+                            # find or create musician
+                            musicianURI = get_musician(musician)                                      
+                            # check if performer exists, create new performer if necessary
+                            performerURI = get_performer(performanceURI, musicianURI, instrumentURI)
+    
+                            add_possible_solo_performer(performanceURI, solo_performance_uri, musicianURI, instrumentURI)       
+                        
+                    else:  
+                        # single musician
+                        musicianURI = get_musician(musician)
+                        performerURI = get_performer(performanceURI, musicianURI, instrumentURI)
+  
                         # add solo performer
                         solo_performer = g.value(solo_performance_uri, DTL.solo_performer)
-                        if solo_performer != None:
-                            solo_musician = g.value(solo_performer, DTL.musician)
-                            soloist_name = g.value(solo_musician, FOAF.name)
-                            logging.info("solo performer %s is set for this solo performance -- deleting", soloist_name)
-                            # delete this solo performer
-                            g.remove( (solo_performance_uri, DTL.solo_performer, solo_performer) )
+                        if solo_performer == performerURI:
+                            logging.debug("same solo performer, keeping")
+                        else:
+                            if solo_performer != None:
+                                solo_musician = g.value(solo_performer, DTL.musician)
+                                soloist_name = g.value(solo_musician, FOAF.name)
+                                logging.info("solo performer %s is set for this solo performance -- deleting", soloist_name)
+                                # delete this solo performer
+                                g.remove( (solo_performance_uri, DTL.solo_performer, solo_performer) )
                             
-                        logging.info("adding solo performer %s playing %s", musician, instrument)
-                        g.add( (solo_performance_uri, DTL.solo_performer, performerURI) )
-                        
+                            logging.info("adding solo performer %s playing %s", musician, instrument)
+                            g.add( (solo_performance_uri, DTL.solo_performer, performerURI) )
+                            
                         # remove possible solo performers
-                        for psperf in g.objects(solo_performance_uri, DTL.possible_solo_performer):
-                            psmusician = g.value(psperf, DTL.musician)
-                            logging.info("removing possible solo performer %s", g.value(psmusician, FOAF.name))
-                        tcount = len(g)
-                        g.remove( (solo_performance_uri, DTL.possible_solo_performer, None) )
-                        if len(g) - tcount > 0:
-                            logging.debug("%i triples removed", len(g) - tcount)
+                        remove_possible_solo_performers(solo_performance_uri)
                                         
 ################################################          
 
